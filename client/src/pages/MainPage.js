33a1a7d61098e5ab6d7f0d042050372a43fb1e2b
beombu <sunil1369@bible.ac.kr>
2021-09-18 04:05:42 +0900

    do_login_can_click

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,16 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useHistory } from "react-router";
+
 
 const MainPage = () => {
+    const history = useHistory();
+
+
+
+const onLinkClick1= (e) =>{
+    e.preventDefault();
+    if(localStorage.sessionId) {
+        history.push("/makeTeam");
+    }
+    else{
+        toast.error("로그인해!");
+        history.push('/');
+    }
+}
+
+const onLinkClick2= (e) =>{
+    e.preventDefault();
+    if(localStorage.sessionId) {
+        history.push("http://localhost:5000/index");
+    }
+    else{
+        toast.error("로그인해!");
+        history.push('/');
+    }
+}
+
     return (
         <div>
             <h2>KBU 예약 시스템!</h2>
-            <Link to="/makeTeam">
-                <span style = {{marginRight: 100}} >팀만들기</span>
-            </Link>
-            <a href="http://localhost:5000/index">
-                팀 참가하기
-            </a>
+                    <Link to="/makeTeam" onClick ={onLinkClick1}>
+                        <span style={{ marginRight: 100 }} >팀만들기</span>
+                    </Link>
+                    <a href="http://localhost:5000/index" onClick={onLinkClick2}>
+                        팀 참가하기
+                    </a>
+
         </div>
     )
 };
