3410352654dba83d616f40428f8436c2536188cd
beombu <sunil1369@bible.ac.kr>
2021-09-30 04:00:01 +0900

    add Toolbar.material_UI

--- client/src/components/ToolBar.js
+++ client/src/components/ToolBar.js
@@ -1,19 +1,36 @@
 import axios from "axios";
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";//해당 url로 이동시켜주는 API
 import { AuthContext } from "../context/AuthContext";
 import { toast } from "react-toastify";
 import { useHistory } from "react-router";
-import "./ToolBar.css";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import HomeIcon from '@mui/icons-material/Home';
+import LogoutIcon from '@mui/icons-material/Logout';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 
 
-const ToolBar = ()=>{
-    const [me,setMe] = useContext(AuthContext);
+const ToolBar = () => {
+    const [me, setMe] = useContext(AuthContext);
     const history = useHistory();
-    
-    const logoutHandler = async() =>{
-        try{
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const logoutHandler = async () => {
+        try {
             await axios.patch("/users/logout");
             setMe();//초기화
             localStorage.removeItem("sessionId");
@@ -21,7 +38,7 @@ const ToolBar = ()=>{
             toast.success("로그아웃 성공");
             history.push('/');
 
-        } catch (err){
+        } catch (err) {
             console.error(err);
             toast.error(err.message);
         }
@@ -29,35 +46,56 @@ const ToolBar = ()=>{
     }
 
     return (
-        <div>
-            <Link to="/">
-            <span>홈</span>
-            </Link>
-
-            {me ? (
-                <>
-                <div className="profile">
-                    <span>마이페이지</span>
-                    <ul className="menu">
-                        <li><Link to="/mypage/getteamlist">나의 팀 모집</Link></li>
-                    </ul>
-                </div>
-                <span onClick={logoutHandler} style={{ float : "right", cursor:"pointer"}}>로그아웃({me.name})</span>
-
-                </>
-                ) : (
-                <>      <Link to="/auth/login">
-                    <span style={{ float: "right" }}>로그인</span>
-                </Link>
-                    <Link to="/auth/register">
-                        <span style={{ float: "right", marginRight: 15 }}>회원가입</span>
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar style={{ background: '#00e676' }} position="static">
+                <Toolbar>
+                    <Link to="/">
+                        <HomeIcon style={{ color: '#757575' }} fontSize="large" sx={{ mr: 2 }} />
                     </Link>
-                </>
-            )}
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        KBU Sports
+                    </Typography>
 
+                    {me ? (
+                        <>
+                            <Button
+                                id="basic-button"
+                                aria-controls="basic-menu"
+                                aria-haspopup="true"
+                                aria-expanded={open ? 'true' : undefined}
+                                onClick={handleClick}
+                            >
+                                마이페이지
+                            </Button>
+                            <Menu
+                                id="basic-menu"
+                                anchorEl={anchorEl}
+                                open={open}
+                                onClose={handleClose}
+                                MenuListProps={{
+                                    'aria-labelledby': 'basic-button',
+                                }}
+                            >
+                                <Link to="/mypage/getteamlist">
+                                <MenuItem>팀모집</MenuItem>
+                                </Link>
+                            </Menu>
+                            <LogoutIcon onClick={logoutHandler} style={{ color: '#757575' }}>로그아웃</LogoutIcon>
 
-
-        </div>
+                        </>
+                    ) : (
+                        <>
+                            <Link to="/auth/login" style={{ textDecoration: 'none' }}>
+                                <Button style={{ color: '#757575' }}>로그인</Button>
+                            </Link>
+                            <Link to="/auth/register" style={{ textDecoration: 'none' }}>
+                                <Button style={{ color: '#757575' }}>회원가입</Button>
+                            </Link>
+                        </>
+                    )}
+                </Toolbar>
+            </AppBar>
+        </Box>
     )
 }
 
